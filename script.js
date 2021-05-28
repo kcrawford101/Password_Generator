f11bd370ad95e6cad0cb3b3b8b4a83ca6ccbacfe
Kevin Crawford <crawfordkevin101@gmail.com>
2021-05-27 20:09:16 -0400

    added passwordGenerate function

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Password Values
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
-var lettersUpper = [];
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -14,6 +14,22 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+
+//Function to generate password
+function generatePassword() {
+  enter = parseInt(prompt("How many characters would you like your password to be? You must choose between 8 and 128"));
+  if (!enter) {
+    alert("Please enter character value!");
+  } else if (enter < 8 || enter > 128) {
+    enter = parseInt(prompt("You need to choose between 8 and 128 characters!"));
+
+  } else {
+    confirmNumber = confirm("Will this contain numbers?");
+    confirmChar = confirm("Will this contain special characters?");
+    confirmUppercase = confirm("Will this contain uppercase letters?");
+    confirmLowercase = confirm("Will this contain lowercase letters?");
+  };
 }
 
 // Add event listener to generate button
